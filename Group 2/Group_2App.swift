1a2dc43a874dc2dacfbbd03306132abb512e4f55
Marina Foko <marina1foko@gmail.com>
2024-07-26 13:27:07 -0400

    Initial Commit

---
+++ Group 2/Group_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Group_2App.swift
+//  Group 2
+//
+//  Created by Marina Foko on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Group_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Group 2UITests/Group_2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Group_2UITestsLaunchTests.swift
+//  Group 2UITests
+//
+//  Created by Marina Foko on 7/26/24.
+//
+
+import XCTest
+
+final class Group_2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
