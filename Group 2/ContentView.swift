7761d8bd6c6a15fbdc3a061ff19e16b45768c5dc
Marina Foko <marina1foko@gmail.com>
2024-07-30 12:02:11 -0400

    Added home page text

--- Group 2/ContentView.swift
+++ Group 2/ContentView.swift
@@ -4,21 +4,55 @@
 //
 //  Created by Marina Foko on 7/26/24.
 //
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name = ""
+    @State private var textTitle = "What is your name"
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello")
+        NavigationStack {
+            VStack(alignment: .center) {
+                Text("Aim Active")
+                    .font(.system(size: 36))
+                    .italic()
+                    .padding(.top, 18) // Add padding from the top to position the text
+                Text("Home").font(.system(size: 30)).padding(.top, 18)
+                Text("     Balance your healthy lifestyle with personalized workout routines set for your fitness goals!").italic().padding(.top, 5)
+                NavigationLink(destination: ContentView()) {
+                        Text("Home")
+                }.padding(.top, 5)
+                NavigationLink(destination: fitnessGoals()) {
+                        Text("Fitness Goals")
+                      }.padding(.top, 5)
+                NavigationLink(destination: fitnessPlan()) {
+                        Text("My Fitness Plan")
+                      }.padding(.top, 5)
+                VStack{
+                    Text(textTitle)
+                    TextField("", text: $name).multilineTextAlignment(.center).font(.title).border(Color.black, width: 1)
+                    Button("Submit Name"){
+                        textTitle = "Welcome, \(name)"
+                        
+                    }.font(.title2).buttonStyle(.borderedProminent).tint(.purple)
+                    
+                   
+                }.padding(.top, 20)
+                Text("Our Cause")
+                Spacer() // Add a spacer to push the content to the bottom
+                
+                Text("")
+                
+                Spacer() // Add a spacer to ensure buttons stay at the bottom
+                
+                    
+                    
+            }
+            .padding() // Add padding around the VStack for better layout
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
 }
+
---
+++ Group 2/fitnessGoals.swift
@@ -0,0 +1,18 @@
+//
+//  fitnessGoals.swift
+//  Group 2
+//
+//  Created by Marina Foko on 7/30/24.
+//
+
+import SwiftUI
+
+struct fitnessGoals: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    fitnessGoals()
+}
